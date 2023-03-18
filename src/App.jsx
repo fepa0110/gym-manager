29733aece4ae6fc5c180be1c25f92e1359fed486
Fabricio <fepafumi97@gmail.com>
2023-03-18 17:49:24 -0300

    feat: :construction: Adding connection to Supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { createClient } from '@supabase/supabase-js'
+import env from "react-dotenv";
 
 import "./App.css";
 
+
 function App() {
+	const supabaseUrl = env.SUPABASE_URL
+	const supabaseKey = env.SUPABASE_KEY
+	const supabase = createClient(supabaseUrl, supabaseKey)
+	
 	const clientes = [
 		{
 			id: 1,
@@ -25,32 +32,41 @@ function App() {
 		<div className="h-full w-full">
 			<nav className="bg-slate-800">
 				<div>
-					<h1 className="text-3xl font-sans  uppercase font-bold text-white h-12">Gym Manager</h1>
+					<h1 className="text-3xl font-sans  uppercase font-bold text-white h-12">
+						Gym Manager
+					</h1>
 				</div>
 			</nav>
 
-      <div className="w-96 p-10">
-        <h1 className="text-3xl">Clientes</h1>
-        <table className="table-auto border-collapse border border-slate-100 w-full hover:border-collapse">
-          <thead>
-            <tr className="bg-slate-800 text-white">
-              <th className="border border-slate-600">Id</th>
-              <th className="border border-slate-600">Cliente</th>
-              <th className="border border-slate-600">Cuota</th>
-            </tr>
-          </thead>
-          <tbody>
-            {clientes.map((cliente)=>{
-              return(
-              <tr key={cliente.id}>
-                <td className="border border-slate-900">{cliente.id}</td>
-                <td className="border border-slate-900">{cliente.nombre}</td>
-                <td className="border border-slate-900">{cliente.cuota}</td>
-              </tr>)
-            })}
-          </tbody>
-        </table>
-      </div>
+			<div className="w-96 p-10 justify-center align-middle">
+				<h1 className="text-3xl">Clientes</h1>
+				<table className="table-auto border-collapse border border-slate-100 w-full hover:border-collapse">
+					<thead>
+						<tr className="bg-slate-800 text-white">
+							<th className="border border-slate-600">Id</th>
+							<th className="border border-slate-600">Cliente</th>
+							<th className="border border-slate-600">Cuota</th>
+						</tr>
+					</thead>
+					<tbody>
+						{clientes.map((cliente) => {
+							return (
+								<tr key={cliente.id}>
+									<td className="border border-slate-900">
+										{cliente.id}
+									</td>
+									<td className="border border-slate-900">
+										{cliente.nombre}
+									</td>
+									<td className="border border-slate-900">
+										{cliente.cuota}
+									</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			</div>
 		</div>
 	);
 }
