c52dfd95c539690d1b7cbb82303fdc0587f77d5e
Fabricio <fepafumi97@gmail.com>
2023-03-28 14:55:24 -0300

    feat: :construction: Added Login UI in HomePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,11 +10,11 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>Gym manager</title>
-    </Head>
-      <Header/>
-      <HomePage/>
+      <Head>
+        <title>Gym manager</title>
+      </Head>
+      {/* <Header/> */}
+      <HomePage />
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,8 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
