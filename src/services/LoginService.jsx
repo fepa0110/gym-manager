ff140afa5aa4430aa2693608160a7e8670ae07c2
Fabricio <fepafumi97@gmail.com>
2023-03-30 04:56:40 -0300

    feat: :sparkles: [LoginService] Added login with email

---
+++ src/services/LoginService.jsx
@@ -0,0 +1,14 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function loginWithEmail(email,password){
+    return await supabase.auth.signInWithPassword({
+        email: email,
+        password: password
+      })
+}
+
+// let { data, error } = 
