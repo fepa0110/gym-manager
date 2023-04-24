155b28d4a54a562a716b1af7786098343e5ceda4
Fabricio <fepafumi97@gmail.com>
2023-04-24 14:19:16 -0300

    style: :lipstick: Added font from Google

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,24 @@
+import Layout from '@/components/Layout'
+import Head from 'next/head'
+
+import { Play } from 'next/font/google'
+
 import '@/styles/globals.css'
+
 
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 
+const play = Play({
+  weight: '400',
+  subsets: ['latin'],
+})
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={play.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
