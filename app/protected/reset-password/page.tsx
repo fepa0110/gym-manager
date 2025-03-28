83549c492e89f235bc6bb9ce34c8b9db53890f48
Fabricio <fepafumi97@gmail.com>
2025-03-27 22:02:28 -0300

    fix: :bug: Resuelto mal llamado de Input en reset-password page

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -5,33 +5,33 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export default async function ResetPassword(props: {
-  searchParams: Promise<Message>;
+	searchParams: Promise<Message>;
 }) {
-  const searchParams = await props.searchParams;
-  return (
-    <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
-      <h1 className="text-2xl font-medium">Reset password</h1>
-      <p className="text-sm text-foreground/60">
-        Please enter your new password below.
-      </p>
-      <Label htmlFor="password">New password</Label>
-      <Input
-        type="password"
-        name="password"
-        placeholder="New password"
-        required
-      />
-      <Label htmlFor="confirmPassword">Confirm password</Label>
-      <Input
-        type="password"
-        name="confirmPassword"
-        placeholder="Confirm password"
-        required
-      />
-      <SubmitButton formAction={resetPasswordAction}>
-        Reset password
-      </SubmitButton>
-      <FormMessage message={searchParams} />
-    </form>
-  );
+	const searchParams = await props.searchParams;
+	return (
+		<form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
+			<h1 className="text-2xl font-medium">Reiniciar contraseña</h1>
+			<p className="text-sm text-foreground/60">
+				Por favor ingresa la nueva contraseña debajo.
+			</p>
+			<Label text="Nueva contraseña" />
+			<Input
+				type="password"
+				name="password"
+				placeholder="New password"
+				required
+			/>
+			<Label text="Confirmar Contraseña" />
+			<Input
+				type="password"
+				name="confirmPassword"
+				placeholder="Confirm password"
+				required
+			/>
+			<SubmitButton formAction={resetPasswordAction}>
+				Reiniciar contraseña
+			</SubmitButton>
+			<FormMessage message={searchParams} />
+		</form>
+	);
 }
